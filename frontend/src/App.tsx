e71c7adf3ffaa914a1c60246bfd9ac26e5c8bdae
Vallabh Rao <vallabhvrao@live.com>
2024-06-01 23:55:16 +0100

    Initial commit - Setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
